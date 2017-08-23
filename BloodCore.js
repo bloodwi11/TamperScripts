705c506ad0366db8a18e02fcb5f8a17d219a8894
bloodwi11 <bloodwi11@gmail.com>
2017-08-23 08:46:23 -0500

    Create BloodCore.js

---
+++ BloodCore.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         BloodCore
+// @namespace    http://powerblocks.net/
+// @version      0.1
+// @description  CorePlugin For my plugins
+// @author       bloodwi11
+// @match        http://www.neopets.com/
+// @grant        none
+// ==/UserScript==
+function getName(nametd) {
+    return nametd.text().replace($nametd.find(".medText").text(), "");
+}
